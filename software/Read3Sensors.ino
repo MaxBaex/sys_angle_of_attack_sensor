2388284b8152fbe0e64b565993d2fae1d79ef757
Jonathan Hahn <helo9@users.noreply.github.com>
2022-07-07 12:40:02 +0200

    [SW]  Move total pressure code into separate file

--- software/Read3Sensors.ino
+++ software/Read3Sensors.ino
@@ -1,15 +1,12 @@
 #include "Tca9548a.h"
+#include "BPS120.h"
 #include "Wire.h"
 
-#define TOTALSENSOR_I2C_ADDR 0x28
 #define DIFFSENSOR_I2C_ADDR 0x38
 
 TCA9548A i2cSwitch(&Wire);
+BPS120 absPressureSensor(&Wire);
 
-// Create array of raw pressure values
-uint16_t pressureRaw[3];
-// Create array of raw temperature values
-uint16_t temperatureRaw[2];
 // Create array of pressure values
 double pressure[3];
 // Create array of temperature values
@@ -85,44 +82,6 @@ uint16_t readRawDiffTemperature(TwoWire *i2c = &Wire) {
     return TempRaw;
 }
 
-uint16_t readRawTotPressure(TwoWire *i2c = &Wire) {
-    // Request for 2-byte data byte(1));
-    i2c->requestFrom(TOTALSENSOR_I2C_ADDR, 2);
-
-    // Wait for data to read
-    // while (Wire.available() == 0);
-
-    // Read first (MSB) data byte and answer with ACK (continue communication)
-    uint8_t msb = i2c->read();
-    // Read second (LSB) data byte and answer with NACK (end communication)
-    uint8_t lsb = i2c->read();
-
-    // Fit <msb, lsb> into pressureRaw variable
-    uint16_t readingRaw = ((uint16_t)msb << 8) | lsb;
-
-    // Extract status
-    uint8_t stat = msb >> 6;
-
-    /*
-      Serial.print("Status: ");
-      Serial.print(stat);
-      Serial.print("\t");
-    */
-
-    // return 0, if status is not 0
-    if (stat != 0) {
-        return 0;
-    }
-
-    // Remove status bits
-    uint16_t pressureRaw = readingRaw & 0x3fff;
-
-    // Return status bits
-    uint16_t statusRaw = readingRaw & 0xC000;
-    // Return raw pressure
-    return pressureRaw;
-}
-
 void setup() {
     Wire.begin();
 
@@ -149,7 +108,7 @@ void setup() {
 void loop() {
     // Read raw pressure values from sensors at hardware I2C
     i2cSwitch.selectChannel(1);
-    pressureRaw[0] = readRawTotPressure();
+    absPressureSensor.fetchSensorValues();
     pressureRaw[1] = readRawDiffPressure();
     temperatureRaw[0] = readRawDiffTemperature();
     i2cSwitch.selectChannel(2);
@@ -157,7 +116,9 @@ void loop() {
     temperatureRaw[1] = readRawDiffTemperature();
     // convert the Raw pressures and temperatures to pressure values in mbar and
     // temperature values in °C
-    pressure[0] = ((float)pressureRaw[0] * 68.1 / 16383 - 33.6) * 30 / 11.8748;
+    pressure[0] =
+        ((float)absPressureSensor.getPressureRaw() * 68.1 / 16383 - 33.6) * 30 /
+        11.8748;
     pressure[1] = ((((float)pressureRaw[1]) * (50) / (16383)) - 24.90) / 0.8;
     pressure[2] = ((((float)pressureRaw[2]) * (50) / (16383)) - 24.96) / 0.8;
 
@@ -168,13 +129,14 @@ void loop() {
     temperature[1] =
         ((float)temperatureRaw[1] * 50 / 2047 - 51.552699387) / 1.06687116;
 
-    Serial.print("Raw pressure values[mbar]: ");
+    Serial.print("Pressure measurements [mbar]: ");
     Serial.print(pressure[0], 4);
     Serial.print(" and ");
     Serial.print(pressure[1], 4);
     Serial.print(" and ");
     Serial.print(pressure[2], 4);
-    Serial.print("  Raw temperature values[°C]: ");
+    Serial.println("");
+    Serial.print("Temperature measruements [°C]: ");
     Serial.print(temperature[0]);
     Serial.print(" and ");
     Serial.print(temperature[1]);
